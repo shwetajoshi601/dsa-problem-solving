30a26567df4a955d79b250234350e90adf36a883
shwetajoshi601 <shwetajoshi601@gmail.com>
2020-01-15 08:06:46 -0800

    Added solution Sum root to leaf BT

---
+++ leetcode/BTSumRootToLeaf.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class BTSumRootToLeaf {
+    public int sumNumbers(TreeNode root) {
+        if(root == null)
+            return 0;
+        
+        // queue to maintain left right nodes
+        Queue<TreeNode> nodes = new LinkedList<TreeNode>();
+        // queue to maintain path sum
+        Queue<Integer> sums = new LinkedList<Integer>();
+        int tot=0;
+        
+        nodes.add(root);
+        sums.add(root.val);
+        
+        while(!nodes.isEmpty()) {
+            TreeNode temp = nodes.poll();
+            int sum = sums.poll();
+            
+            // base case - leaf node
+            if(temp.left == null && temp.right == null)
+                tot+=sum;
+            
+            // process the left child
+            if(temp.left != null) {
+                nodes.add(temp.left);
+                // between each node and its children there will be a diff
+                // of a factor of 10
+                sums.add((sum*10) + temp.left.val);
+            }
+            
+            // process the right child
+            if(temp.right != null) {
+                nodes.add(temp.right);
+                sums.add((sum*10) + temp.right.val);
+            }
+        }
+        
+        return tot;
+    }
+}
