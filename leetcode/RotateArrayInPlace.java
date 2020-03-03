9824e913f1ca9d4b268487ae36dc45bc48ab1b5f
shwetajoshi601 <shwetajoshi601@gmail.com>
2020-03-03 13:28:45 +0000

    Added solution - rotate matrix in place - leetcode

---
+++ leetcode/RotateArrayInPlace.java
@@ -0,0 +1,48 @@
+class RotateArrayInPlace {
+    
+    // function to print the matrix
+    void printMatrix(int[][] matrix) {
+        for(int i=0; i<matrix.length;i++) {
+            System.out.println();
+            for(int j=0; j<matrix.length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+        }
+    }
+    
+    public void rotate(int[][] matrix) {
+        
+        // take the transpose of the matrix
+        // matrix[i][j] = matrix[j][i]
+        
+        for(int i=0; i<matrix.length; i++) {
+            for(int j=i; j<matrix.length;j++) {
+                int temp=matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        // reverse each row of the matrix
+        // the resulting matrix is the rotated version of the array
+        
+        for(int i=0; i<matrix.length; i++) {
+            // read each row
+            int[] row = matrix[i];
+            
+            // two pointers - start and end
+            int l=0, h=row.length-1;
+            
+            // swap the corresponding elements from beginning and end
+            while(l<=h) {
+                int temp = row[l];
+                row[l] = row[h];
+                row[h] = temp;
+                l++;
+                h--;
+            }
+        }
+        
+        printMatrix(matrix);
+    }
+}
