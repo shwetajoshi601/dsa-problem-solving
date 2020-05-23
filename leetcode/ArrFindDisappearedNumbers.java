30eeda73686724417565993544b08bf1c4089118
shwetajoshi601 <shwetajoshi601@gmail.com>
2020-05-23 12:43:55 +0100

    Added approach 2 for solution - leetcode

--- leetcode/ArrFindDisappearedNumbers.java
+++ leetcode/ArrFindDisappearedNumbers.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class ArrFindDisappearedNumbers {
+    // Approach-1: Using set
     public List<Integer> findDisappearedNumbers(int[] nums) {
         Set<Integer> set=new HashSet<Integer>();
         List<Integer> result=new ArrayList<Integer>();
@@ -15,6 +16,28 @@ public class ArrFindDisappearedNumbers {
             if(!set.contains(j))
                 // add to the result
                 result.add(j);
+        }
+        
+        return result;
+    }
+
+    // Approach-2: In place using negation as a marking mechanism for elements that are present
+    public List<Integer> findDisappearedNumbersInPlace(int[] nums) {
+        List<Integer> result=new ArrayList<Integer>();
+        
+        for(int i=0; i<nums.length; i++) {
+            // check if the number at ith position is positive
+            if(nums[Math.abs(nums[i])-1] > 0) {
+                // negate the number
+                // e.g.: if nums[0]=4, then change number at position 4 (index 3) to negative
+                nums[Math.abs(nums[i])-1]= -nums[Math.abs(nums[i])-1];
+            }
+        }
+        
+        // at the end, collect the numbers that remain positive
+        for(int j=0; j<nums.length;j++) {
+            if(nums[j] > 0)
+                result.add(j+1);
         }
         
         return result;
