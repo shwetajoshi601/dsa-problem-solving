1ad1aed12d65e90530ece8a0bb3981da4b2ef506
shwetajoshi601 <shwetajoshi601@gmail.com>
2020-06-15 08:20:48 +0100

    Added solution - two sum - leetcode

--- leetcode/ArrTwoSum.java
+++ leetcode/ArrTwoSum.java
@@ -1,4 +1,6 @@
-class TwoSum {
+import java.util.*;
+
+class ArrTwoSum {
     public int[] twoSum(int[] nums, int target) {
         // map<value, index>
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
@@ -20,5 +22,29 @@ class TwoSum {
             }
         }
         return result;
+    }
+
+    public int[] twoSumSingleLoop(int[] nums, int target) {
+        int[] res={-1, -1};
+        
+        if(nums.length == 0) return res;
+        
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        
+        for(int i=0; i<nums.length; i++) {
+            // a+b=target, a=target-b
+            int num2=target-nums[i];
+            // if the second element of the pair is present
+            // also check if it is not the same element, e.g. [3,2,4], target=6 -> map.get would return index of 3 for element 3
+            if(map.containsKey(num2) && map.get(num2) != i) {
+                res[0] = i;
+                res[1] = map.get(num2);
+                break;
+            }
+
+            map.put(nums[i], i);
+        }
+        
+        return res;
     }
 }
