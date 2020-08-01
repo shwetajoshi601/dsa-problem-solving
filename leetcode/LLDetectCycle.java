548c316478a4761078759c2ce1869cd203c34212
shwetajoshi601 <shwetajoshi601@gmail.com>
2020-08-01 14:28:34 +0100

    Added solution - leetcode - linked list cycle detection

---
+++ leetcode/LLDetectCycle.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        // Floyd's cycle detection algorithm
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            // slow and fast would meet if the list contains a cycle
+            if(slow == fast)
+                return true;
+        }
+        
+        // no cycle
+        return false;
+    }
+}
