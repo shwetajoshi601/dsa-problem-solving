7a3d94e316585903eeeb14e2e78a6cb2ab411af6
shwetajoshi601 <shwetajoshi601@gmail.com>
2021-02-03 22:29:33 +0000

    Added solution - find duplicate - leetcode

--- leetcode/ArrFindDuplicateNumber.java
+++ leetcode/ArrFindDuplicateNumber.java
@@ -26,4 +26,35 @@ class ArrFindDuplicateNumber {
         // return the element
         return slow;
     }
+
+    // Soln 2: Using Swap Sort - we know that in the range 1 to N, there is only 1 repeated number
+    void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    public int findDuplicateSwapSort(int[] nums) {
+        int i = 0;
+        
+        // element at pos 0 is 1, at pos 1 is 2, hence swap the elements if they are not in their right pos
+        while(i<nums.length) {
+            if(nums[i] != nums[nums[i]-1])
+                swap(nums, i, nums[i]-1);
+            else
+                i++;
+        }
+        
+        // now check if all elems are in their correct position
+        // the one in incorrect position is the repeated number
+        int rep = -1;
+        for(i=0; i<nums.length; i++) {
+            if(nums[i] != i+1) {
+                rep = nums[i];
+                break;
+            }
+        }
+        
+        return rep;
+    }
 }
