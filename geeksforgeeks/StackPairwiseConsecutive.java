10a3a2d727cd115a5033e8302006de3708a7e194
shwetajoshi601 <shwetajoshi601@gmail.com>
2021-07-05 23:46:10 +0100

    Gfg - Pairwise cosecutive - stack

---
+++ geeksforgeeks/StackPairwiseConsecutive.java
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+
+class StackPairwiseConsecutive
+{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            Stack<Integer> st = new Stack<Integer>();
+            int n = sc.nextInt();
+            int temp = 0;
+            for(int i = 0; i < n; i++)
+            {
+                temp = sc.nextInt();
+                st.push(temp);
+            }
+            GFG gfg = new GFG();
+            if(gfg.pairWiseConsecutive(st) == true)
+            System.out.println("Yes");
+            else
+            System.out.println("No");
+            
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+// your task is to complete the function
+// function should return true/false or 1/0
+class GFG
+{
+    public static boolean pairWiseConsecutive(Stack<Integer> st)
+    {
+       if(st.size() == 1)
+            return true;
+            
+       if(st.size() % 2 > 0)
+            st.pop();
+            
+        while(!st.isEmpty() && st.size() > 1) {
+            int num1 = st.pop();
+            int num2 = st.pop();
+            
+            if(Math.abs(num1-num2) != 1)
+                return false;
+        }
+       
+       return true;
+	}
+}
