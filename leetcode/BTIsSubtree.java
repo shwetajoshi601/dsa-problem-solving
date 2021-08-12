d0d996fe8c598031e6d1799efa7635986bf55edd
shwetajoshi601 <shwetajoshi601@gmail.com>
2021-08-12 22:23:40 +0100

    Leetcode - is subtree

---
+++ leetcode/BTIsSubtree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BTIsSubtree {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root == null)
+            return false;
+        
+        if(subRoot == null)
+            return true;
+        
+        if(isIdentical(root, subRoot))
+            return true;
+        
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+    
+    boolean isIdentical(TreeNode root1, TreeNode root2) {
+        if(root1 == null && root2 == null)
+            return true;
+        
+        if(root1 == null || root2 == null)
+            return false;
+        
+        return root1.val == root2.val && isIdentical(root1.left, root2.left) && isIdentical(root1.right, root2.right);
+    }
+}
