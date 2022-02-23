02471140620f398f836184f6c556deb4c71412e1
shwetajoshi601 <shwetajoshi601@gmail.com>
2022-02-23 19:39:54 +0000

    Trees - symmetric tree recursive

--- leetcode/BTSymmetricTree.java
+++ leetcode/BTSymmetricTree.java
@@ -44,4 +44,26 @@ class BTSymmetricTree {
         }
         return true;
     }
+
+    // recursive
+    public boolean isSymmetricRecursive(TreeNode root) {
+        if(root == null)
+            return true;
+        
+        return isMirror(root.left, root.right);
+    }
+    
+    // helper function that checks if two trees are identical (in this case mirrors)
+    public boolean isMirror(TreeNode root1, TreeNode root2) {
+        // if one of the roots is null -> false, if both are null -> true
+        if(root1 == null || root2 == null)
+            return root1 == root2;
+        
+        // if the values are unequal -> false
+        if(root1.val != root2.val)
+            return false;
+        
+        // since we are checking for symmetry, the left child of root1 should be equal to the right child of root2, and right child of root1 should be equal to the left child of root2
+        return isMirror(root1.left, root2.right) && isMirror(root1.right, root2.left);
+    }
 }
